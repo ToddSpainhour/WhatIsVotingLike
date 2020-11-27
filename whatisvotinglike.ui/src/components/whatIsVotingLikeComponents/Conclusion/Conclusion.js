139617855014aba907d788703451449b8b9dec90
Todd Spainhour <toddspainhour@gmail.com>
2020-11-26 22:49:49 -0600

    added Link buttons to advance to next component

--- whatisvotinglike.ui/src/components/whatIsVotingLikeComponents/Conclusion/Conclusion.js
+++ whatisvotinglike.ui/src/components/whatIsVotingLikeComponents/Conclusion/Conclusion.js
@@ -1,5 +1,7 @@
 import React from 'react';
 
+import { Link } from 'react-router-dom';
+
 import './Conclusion.scss';
 
 class Conclusion extends React.Component {
@@ -7,6 +9,7 @@ class Conclusion extends React.Component {
     return (
       <div className="Conclusion">
         <h5>Conclusion</h5>
+        <Link to="/home" className="btn btn-warning">To Home</Link>
       </div>
     );
   }
