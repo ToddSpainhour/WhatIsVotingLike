07d49375f9909a13ee378c4ee34304ba9c7d6670
Todd Spainhour <toddspainhour@gmail.com>
2020-12-05 15:28:20 -0600

    Slides endpoint returning all slides or by TrainingModuleId

---
+++ WhatIsVotingLike/Controllers/SlidesController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using WhatIsVotingLike.Data;
+using WhatIsVotingLike.Models;
+
+namespace WhatIsVotingLike.Controllers
+{
+    [Route("api/slides")]
+    [ApiController]
+    public class SlidesController : ControllerBase
+    {
+
+        SlideRepository _repo;
+
+
+        public SlidesController(SlideRepository repo)
+        {
+            _repo = repo;
+        }
+
+
+        [HttpGet("{trainingModuleId}")]
+        public IActionResult GetAllSlidesByTrainingModuleId(int trainingModuleId)
+        {
+            var SlidesFromTrainingModule = _repo.GetAllSlidesForModule(trainingModuleId);
+
+            return Ok(SlidesFromTrainingModule);
+        }
+
+
+        [HttpGet]
+        public IActionResult GetAllSlides()
+        {
+            var AllSlides = _repo.GetAll();
+
+            return Ok(AllSlides);
+        }
+    }
+}
---
+++ WhatIsVotingLike/Models/Slides.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WhatIsVotingLike.Models
+{
+    public class Slides
+    {
+        public int TrainingModuleId { get; set; }
+        public int SlideId { get; set; }
+        public int SlideOrderPosition { get; set; }
+        public string SlideTitle { get; set; }
+        public string SlideBodyText { get; set; }
+        public string ImageUrl { get; set; }
+        public string Question { get; set; }
+        public string CorrectAnswer { get; set; }
+        public string IncorrectAnswer1 { get; set; }
+        public string IncorrectAnswer2 { get; set; }
+        public string IncorrectAnswer3 { get; set; }
+        public bool IsComplete { get; set; }
+    }
+}
