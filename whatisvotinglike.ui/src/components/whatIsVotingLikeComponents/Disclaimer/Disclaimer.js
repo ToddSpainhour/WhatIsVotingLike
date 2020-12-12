58722729462f67213b90662a13621a7384596ee9
Todd Spainhour <toddspainhour@gmail.com>
2020-12-12 11:52:00 -0600

    displaying info from props and added checkbox to change completed state

--- whatisvotinglike.ui/src/components/whatIsVotingLikeComponents/Disclaimer/Disclaimer.js
+++ whatisvotinglike.ui/src/components/whatIsVotingLikeComponents/Disclaimer/Disclaimer.js
@@ -6,6 +6,8 @@ import './Disclaimer.scss';
 
 class Disclaimer extends React.Component {
   render() {
+    if (!this.props.slides[0]) return null;
+
     let nextButton;
     if (this.props.disclaimerComplete === true) {
       nextButton = <Link to='./intro' className="btn btn-light m-3 next-button">Active Next</Link>;
@@ -13,18 +15,22 @@ class Disclaimer extends React.Component {
       nextButton = <button className="btn btn-light m-3 disabled-next-button">Disabled Next</button>;
     }
     return (
-      <div className="Disclaimer">
-        <h5>Disclaimer</h5>
-        <h6>Some slides will disable the next button until the user performs some action.</h6>
-        <p>Current disclaimerComplete status: {this.props.disclaimerComplete.toString()}</p>
+      <main className="Disclaimer">
+        <h2>{this.props.slides[1].slideTitle}</h2>
 
-        <button onClick={this.props.ChangeDisclaimerCompletedStatus} >Click to fulfill slide requirements</button>
+        <article>
+          <h5>{this.props.slides[1].slideBodyText}</h5>
+          <input type="checkbox" id="disclaimer-checkbox" onChange={this.props.ChangeDisclaimerCompletedStatus} name="disclaimer-checkbox"/>
+          <label htmlFor="disclaimer-checkbox">I Understand</label>
+        </article>
 
-        <div className="module-navigation d-flex justify-content-sm-end justify-content-center">
+        <img src={this.props.slides[1].imageUrl} className="disclaimer-image" alt="Polling Location"/>
+
+        <nav className="module-navigation d-flex justify-content-sm-end justify-content-center">
           <Link to='./home' className="btn btn-light m-3">Previous</Link>
           {nextButton}
-        </div>
-      </div>
+        </nav>
+      </main>
     );
   }
 }
