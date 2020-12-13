d65fd783c8d9ae589977665d7b68952e16986e09
Todd Spainhour <toddspainhour@gmail.com>
2020-12-13 10:02:54 -0600

    displaying data from props and added div core content

--- whatisvotinglike.ui/src/components/whatIsVotingLikeComponents/Intro/Intro.js
+++ whatisvotinglike.ui/src/components/whatIsVotingLikeComponents/Intro/Intro.js
@@ -16,12 +16,14 @@ class Intro extends React.Component {
     return (
       <main className="Intro">
         <h2>{this.props.slides[0].slideTitle}</h2>
-
+        <div className="intro-content-container">
         <article>
           <h5>{this.props.slides[0].slideBodyText}</h5>
         <button onClick={this.props.ChangeIntroCompletedStatus} className='btn btn-light'>Let's Get Started</button>
         </article>
 
+        <img src={this.props.slides[0].imageUrl} alt="Voter holding a sign that says Vote."/>
+        </div>
         <nav className="module-navigation d-flex justify-content-sm-end justify-content-center">
           <Link to='./disclaimer' className="btn btn-light m-3 previous-button">Previous</Link>
           {nextButton}
