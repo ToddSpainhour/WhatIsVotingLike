dbc0e36de1e917f7724686e500f8a7eb565321c3
Todd Spainhour <toddspainhour@gmail.com>
2020-12-13 21:17:29 -0600

    added Slider component into Intro component

--- whatisvotinglike.ui/src/components/whatIsVotingLikeComponents/Intro/Intro.js
+++ whatisvotinglike.ui/src/components/whatIsVotingLikeComponents/Intro/Intro.js
@@ -2,6 +2,8 @@ import React from 'react';
 
 import { Link } from 'react-router-dom';
 
+import Slider from '../../commonComponents/Slider/Slider';
+
 import './Intro.scss';
 
 class Intro extends React.Component {
@@ -17,13 +19,15 @@ class Intro extends React.Component {
       <main className="Intro">
         <h2>{this.props.slides[0].slideTitle}</h2>
         <div className="intro-content-container">
-        <article>
-          <h5>{this.props.slides[0].slideBodyText}</h5>
-        <button onClick={this.props.ChangeIntroCompletedStatus} className='btn btn-light'>Let's Get Started</button>
-        </article>
+          <article>
+            <h5>{this.props.slides[0].slideBodyText}</h5>
+            <Slider/>
+          <button onClick={this.props.ChangeIntroCompletedStatus} className='btn btn-light'>Let's Get Started</button>
+          </article>
 
-        <img src={this.props.slides[0].imageUrl} alt="Voter holding a sign that says Vote."/>
+          <img src={this.props.slides[0].imageUrl} alt="Voter holding a sign that says Vote."/>
         </div>
+
         <nav className="module-navigation d-flex justify-content-sm-end justify-content-center">
           <Link to='./disclaimer' className="btn btn-light m-3 previous-button">Previous</Link>
           {nextButton}
