3febcb7b4e986f9ebad38a2c07a38e65b624b61b
Todd Spainhour <toddspainhour@gmail.com>
2020-12-19 11:35:24 -0600

    added question, dynamic and genaric response upon selection

--- whatisvotinglike.ui/src/components/whatIsVotingLikeComponents/CheckIn/CheckIn.js
+++ whatisvotinglike.ui/src/components/whatIsVotingLikeComponents/CheckIn/CheckIn.js
@@ -40,36 +40,47 @@ class CheckIn extends React.Component {
 
     let AnswerResponse;
     if (this.state.selectedAnswerId !== '' && this.state.selectedAnswerId === 'collegestudentidwithyourphoto') {
-      AnswerResponse = <h6>That's Correct. College student Id's can not be used as a photo id when voting in Tennessee.</h6>;
+      AnswerResponse = <div>
+                          <h6>That's Correct.</h6>
+                          <h6>A driver’s license or photo ID issued by the Tennessee Department of Safety and Homeland Security as well as photo IDs issued by
+                              Tennessee state government or the federal government are acceptable even if they are expired. But college student IDs are not.
+                          </h6>
+                      </div>;
     } else if (this.state.selectedAnswerId === '') {
       AnswerResponse = '';
     } else {
-      AnswerResponse = <h6>That's incorrect. The only id mentioned that you couldn't use when going to is a college student id</h6>;
+      AnswerResponse = <div>
+                        <h6>That's incorrect.</h6>
+                        <h6>A driver’s license or photo ID issued by the Tennessee Department of Safety and Homeland Security as well as photo IDs issued by
+                            Tennessee state government or the federal government are acceptable even if they are expired. But college student IDs are not.
+                        </h6>
+                       </div>;
     }
+
     let nextButton;
     if (this.props.checkInComplete === true) {
       nextButton = <Link to='./votingMachine' className="btn btn-light m-3 next-button">Active Next</Link>;
     } else {
       nextButton = <button className="btn btn-light m-3 disabled-next-button">Disabled Next</button>;
     }
+
     return (
       <main className="CheckIn">
         <h2>{this.props.slides[5].slideTitle}</h2>
         <article>
           <h5>{this.props.slides[5].slideBodyText}</h5>
           <div className="checkin-content-container">
+            <h5>Out of the options below, which is NOT a valid photo ID when voting in Tennessee? Drag and drop your answer.</h5>
             <div>
-              <img src={this.props.slides[4].imageUrl} alt="Poll Worker Checking ID"/>
             </div>
           </div>
         </article>
-        <button onClick={this.props.ChangeCheckInCompletedStatus} >Click to fulfill slide requirements</button>
 
         <div className="drag-and-drop-container">
-        <div id="drop-zone" className="droppable col-6" onDragOver={(e) => e.preventDefault()} onDrop={(e) => this.onDrop(e)}>Drop stuff here</div>
-        <div>{AnswerResponse}</div>
-        <div draggable className="draggable col-6">{idToShow}</div>
+          <div draggable className="draggable col-6">{idToShow}</div>
+          <div id="drop-zone" className="droppable col-6" onDragOver={(e) => e.preventDefault()} onDrop={(e) => this.onDrop(e)}>Drop and drop your answer here.</div>
         </div>
+        <div>{AnswerResponse}</div>
 
         <div className="module-navigation d-flex justify-content-sm-end justify-content-center">
           <Link to='./insidePollingPlace' className="btn btn-light m-3">Previous</Link>
