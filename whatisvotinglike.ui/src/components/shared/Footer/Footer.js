947d1661d861bbb68e1a10100ba54db08e82a6fa
Todd Spainhour <toddspainhour@gmail.com>
2020-12-21 18:34:00 -0600

    added link and styling to footer

--- whatisvotinglike.ui/src/components/shared/Footer/Footer.js
+++ whatisvotinglike.ui/src/components/shared/Footer/Footer.js
@@ -6,7 +6,8 @@ class Footer extends React.Component {
   render() {
     return (
       <div className="Footer">
-        <h5>Footer</h5>
+        <p>See More at <a href="https://toddspainhour.com/">ToddSpainhour.com</a></p>
+        {/* <p>Thanks for stopping by!</p> */}
       </div>
     );
   }
