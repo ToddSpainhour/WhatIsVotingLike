e0a3698711734c64a05b5ebbc23aec3056dd36e3
Todd Spainhour <toddspainhour@gmail.com>
2020-12-22 20:28:38 -0600

    moved external links to register into Conclusion component

--- whatisvotinglike.ui/src/components/whatIsVotingLikeComponents/Conclusion/Conclusion.js
+++ whatisvotinglike.ui/src/components/whatIsVotingLikeComponents/Conclusion/Conclusion.js
@@ -10,6 +10,18 @@ class Conclusion extends React.Component {
       <div className="Conclusion">
         <h5>Conclusion</h5>
         <Link to='./home' className="btn btn-small complete-training-button">Complete Training</Link>
+
+        <div className="register-button-container">
+              <div>
+                <h6>Need to register to vote in Tennessee?</h6>
+                <a className="btn btn-light" href="https://ovr.govote.tn.gov/" target="_blank" rel="noreferrer nofollow">Visit Tennessee's Online Voter Registration Portal.</a>
+              </div>
+              <div>
+                <h6>Already registered? Confirm your registration by clicking the link below.</h6>
+                <a className="btn btn-light" href="https://tnmap.tn.gov/voterlookup/" target="_blank" rel="noreferrer nofollow">Confirm Your Registration in Tennessee.</a>
+              </div>
+            </div>
+
       </div>
     );
   }
