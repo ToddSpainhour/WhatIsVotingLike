3add809e2a348f508955d9c9f507f5edd1f1d2a6
Todd Spainhour <toddspainhour@gmail.com>
2021-01-05 20:31:52 -0600

    added dynamic submit button to Slider component

--- whatisvotinglike.ui/src/components/commonComponents/Slider/Slider.js
+++ whatisvotinglike.ui/src/components/commonComponents/Slider/Slider.js
@@ -4,6 +4,13 @@ import './Slider.scss';
 
 class Slider extends React.Component {
   render() {
+    let dynamicSubmitButton;
+    if (this.props.submittedAnswer === false) {
+      dynamicSubmitButton = <button onClick={this.props.submitAnswer}className="btn btn-light">Submit Answer</button>;
+    } else {
+      dynamicSubmitButton = <button disabled className="btn btn-light">Submitted</button>;
+    }
+
     return (
       <div className="Slider">
         <h5>What percentage of voters in Tennessee choose to vote in person?</h5>
@@ -12,7 +19,7 @@ class Slider extends React.Component {
           <output id="output"></output>
           <div className="slider-result">{this.props.sliderValue}%</div>
           </div>
-          <button onClick={this.props.submitAnswer}className="btn btn-light">Submit Answer</button>
+          { dynamicSubmitButton }
       </div>
     );
   }
