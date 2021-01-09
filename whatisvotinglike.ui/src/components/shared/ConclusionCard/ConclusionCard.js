a865d54c44d785831dca165ed3fe0ab5a08099a0
Todd Spainhour <toddspainhour@gmail.com>
2021-01-09 15:30:24 -0600

    wrapped external link button in anchor tag which opens in new tab once clicked

--- whatisvotinglike.ui/src/components/shared/ConclusionCard/ConclusionCard.js
+++ whatisvotinglike.ui/src/components/shared/ConclusionCard/ConclusionCard.js
@@ -7,7 +7,9 @@ class ConclusionCard extends React.Component {
     return (
       <div className="ConclusionCard col-3">
         <h6>{this.props.title}</h6>
-        <button href={this.props.url} className="btn btn-small btn-light">{this.props.btnText}</button>
+          <a href={this.props.url} target="_blank" rel="noreferrer">
+            <button className="btn btn-small btn-light">{this.props.btnText}</button>
+          </a>
       </div>
     );
   }
